30b8102bfbd9c5a9d433c53dd79ffe3bbddacc1a
Aditya Dixit <qwertyuiop.dixit3@gmail.com>
2019-06-22 09:46:23 +0530

    Working temporarily
    fire away

--- gitlist.sh
+++ gitlist.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 read -p "Enter your github repo URL: " url
-echo "$url"
 read -p "Enter the folder name to clone to: " dest
 git clone $url $dest
 read -p "Enter the folder name to generate wordlist: " fold
-cd ./$dest/$fold
-ls > "$fold"
+mkdir results
+ls $dest/$fold > ./results/out
 echo "let's fuzz"
-read -p "Enter the URL to FUZZ" fuzzme
-wfuzz -c -z file,$fold --hc 404 $fuzzme/FUZZ > $fold-final
-echo "Open the file $fold-final for the final URL's"
-	
+read -p "Enter the URL to FUZZ: " fuzzme
+wfuzz -c -z file,./results/out --hc 404 $fuzzme/FUZZ | tee finalout
+echo "Open the file in ./finalout for the final URL's"
